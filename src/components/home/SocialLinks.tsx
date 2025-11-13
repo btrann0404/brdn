c195e2de7f50a5901f12b502ebcd702d6bc96c79
btrann0404 <tranbrandon04@gmail.com>
2025-11-12 16:18:37 -0800

    fixed email link

--- src/components/home/SocialLinks.tsx
+++ src/components/home/SocialLinks.tsx
@@ -34,7 +34,7 @@ export default function SocialLinks() {
         </a>
         <span className="text-white/20">·</span>
         <a 
-          href="tranbrandon04@gmail.com" 
+          href="mailto:tranbrandon04@gmail.com" 
           className="text-white/50 font-light hover:text-white/80 active:text-white/80 transition-colors duration-300"
         >
           <GlitchText text="EMAIL" delay={0} speed={50} />
